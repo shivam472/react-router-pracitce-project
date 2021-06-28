2ebe117eb09ac67e9b2b845ffa89dca0be6bda02
shivam472 <shivamraj472@gmail.com>
2021-06-28 23:15:34 +0530

    preventing unwanted route transitions with the "

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,10 +1,13 @@
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { Prompt } from "react-router";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -20,27 +23,39 @@ const QuoteForm = (props) => {
   }
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <React.Fragment>
+      <Prompt
+        when={isFocused}
+        message="Are you sure you want to leave this page? All your data will be lost"
+      />
+      <Card>
+        <form
+          onFocus={() => setIsFocused(true)}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={() => setIsFocused(false)} className="btn">
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </React.Fragment>
   );
 };
 
