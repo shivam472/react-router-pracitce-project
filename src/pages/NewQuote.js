62c2a1594523c514ae17ae434fcac51d85bae924
shivam472 <shivamraj472@gmail.com>
2021-06-28 23:01:24 +0530

    Implementing programatic navigation with react-router

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
+    history.push("/quotes");
     console.log(quoteData);
   };
   return (
